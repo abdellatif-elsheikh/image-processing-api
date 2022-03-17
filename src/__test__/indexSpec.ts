755d1e4615b47c56f05b8dc6cae01fd339473818
abdellatif-elsheikh <elshikh532@gmail.com>
2022-03-17 16:27:49 +0200

    use middlewares and create main route

--- src/__test__/indexSpec.ts
+++
@@ -1,7 +0,0 @@
-import myFunc from '../index';
-
-describe('test main', () => {
-	it('should return array without second item', () => {
-		expect(myFunc([1, 5, 4, 6, 7])).not.toContain(4);
-	});
-});
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
-const myFunc = (arr: (string | number)[]): (string | number)[] => {
-	arr.splice(2, 1);
-	return arr;
-};
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import router from './routes';
+// import sharp from 'sharp';
 
-console.log(myFunc([1, 5, 4, 7]));
+// create app instance
+const app = express();
+const port = 5000;
 
-export default myFunc;
+// use middleware for app
+app.use(helmet());
+app.use(morgan('combined'));
+
+// create rate limit object
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 15 minutes
+	max: 60,
+	standardHeaders: true,
+	legacyHeaders: false,
+	message: 'too many requests please tray agin after 10 minutes',
+});
+// use rate limiter lib
+app.use(limiter);
+
+// create router
+app.use('/api/image', router);
+
+app.listen(port, () => {
+	console.log(
+		`please open the flowing link to view the project http://localhost/${port}`
+	);
+});
